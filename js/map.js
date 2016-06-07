d00ef3e9c5151e1c7bc2e09e1750ca54aa84308c
João Júnior <jjjuniorjr2@gmail.com>
2016-06-07 13:15:41 -0300

    Disponibilização
    Disponibilização de arquivos do site

---
+++ js/map.js
@@ -0,0 +1,56 @@
+jQuery(function($){
+
+	var longitude = -18.909378;
+	var latitude = -48.269404;
+	var canvas = "map";
+
+	
+	function randing_map(canvas, lan, lat){			
+			var myLatlng = new google.maps.LatLng(lan,lat);
+			var myOptions = {
+						zoom: 15,
+						center: myLatlng,
+						mapTypeId: google.maps.MapTypeId.ROADMAP,
+						maxZoom   : 20,
+    					disableDefaultUI: true
+					}			
+			var map = new google.maps.Map( document.getElementById(canvas), myOptions );
+			var marker = new google.maps.Marker({
+			    position : myLatlng,
+			    map      : map,
+			    icon     : "img/point.png"
+			});
+			var styles = [
+			  {
+			    featureType: "all",
+			    stylers: [
+			      { saturation: -80 }
+			    ]
+			  },{
+			    featureType: "road.arterial",
+			    elementType: "geometry",
+			    stylers: [
+			      { hue: "#00ffee" },
+			      { saturation: 50 }
+			    ]
+			  },{
+			    featureType: "poi.business",
+			    elementType: "labels",
+			    stylers: [
+			      { visibility: "off" }
+			    ]
+			  }
+			];	
+			var infowindow = new google.maps.InfoWindow({
+				content:"<div class='map_adresse'><div class='map_address'><span class='address'>Endereço: </span>Avenida Floriano Peixoto 1767 - Sala 4, Uberlândia - MG</div> <div class='map_tel'><span class='tel'>Telefone: </span>(34) 3305-6111 </div><div class='map_email'><span class='email'>Email: </span> <a href='mailto:contato@aprendendotestar.com.br'>contato@aprendendotestar.com.br</a></div></div>"
+			});	
+			
+			map.setOptions({styles: styles});
+
+			google.maps.event.addListener(marker, 'click', function() {
+			  infowindow.open(map,marker);
+			});
+	}
+	randing_map(canvas, longitude, latitude);
+
+});
